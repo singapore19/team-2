5f6794a3ebeab5f201edb4d2d868636934dc721b
zoeboya <36061695+zoeboya@users.noreply.github.com>
2019-10-04 13:54:18 +0800

    user add job

--- Frontend/UserApplication/mobile/lib/src/screens/add_job.dart
+++ Frontend/UserApplication/mobile/lib/src/screens/add_job.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 // Define a custom Form widget.
 import 'package:flutter/material.dart';
 
@@ -52,7 +53,12 @@ class MyCustomFormState extends State<MyCustomForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+
+          // 1 from address
           TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your address (from)'
+            ),
             validator: (value) {
               if (value.isEmpty) {
                 return 'Please enter some text';
@@ -64,16 +70,127 @@ class MyCustomFormState extends State<MyCustomForm> {
             padding: const EdgeInsets.symmetric(vertical: 16.0),
           ),
 
+          // 2 from postal code
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your postal code (from)'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+          // 3 to address
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your address (to)'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+          // 4 to postal code
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your postal code (to)'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+          // 5 type
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your type'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+
+          // 7 msc volume
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your volume'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+          // 8 msc number
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your number'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
+
+
+          // 9 msc time
+          TextFormField(
+            decoration: InputDecoration(
+            labelText: 'Enter your time (optional)'
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+          ),
         
-        RaisedButton(
-          onPressed: () {
-            // Validate returns true if the form is valid, or false
-            // otherwise.
-            if (_formKey.currentState.validate()) {
-              // If the form is valid, display a Snackbar.
-              Scaffold.of(context)
-                  .showSnackBar(SnackBar(content: Text('Processing Data')));
-            }
+
+
+          RaisedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false
+              // otherwise.
+              if (_formKey.currentState.validate()) {
+                // If the form is valid, display a Snackbar.
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+
+            //submit to backend
           },
           child: Text('Submit'),
         ),
